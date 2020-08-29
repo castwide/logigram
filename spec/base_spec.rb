70731615d230716b19e548c7dcb4e1167e26630c
Fred Snyder <fsnyder@castwide.com>
2020-08-29 05:46:01 -0400

    Base specs

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,11 +1,50 @@
 RSpec.describe Logigram::Base do
-  it 'accepts constraints' do
-    klass = Class.new(Logigram::Base) do
-      constrain 'color', ['red', 'green', 'blue']
+  describe 'initialization' do
+    it 'accepts constraints' do
+      klass = Class.new(Logigram::Base) do
+        constrain 'color', ['red', 'green', 'blue']
+      end
+      # @type [Logigram::Base]
+      puzzle = klass.new(['dog', 'cat', 'pig'])
+      expect(puzzle.terms).to eq(['color'])
+      expect(puzzle.constraints['color'].values).to eq(['red', 'green', 'blue'])
     end
-    # @type [Logigram::Base]
-    puzzle = klass.new(['the dog', 'the cat', 'the pig'])
-    expect(puzzle.terms).to eq(['color'])
-    expect(puzzle.constraints['color'].values).to eq(['red', 'green', 'blue'])
+
+    it 'accepts a solution' do
+      klass = Class.new(Logigram::Base) do
+        constrain 'color', ['red', 'green', 'blue']
+      end
+      # @type [Logigram::Base]
+      puzzle = klass.new(['dog', 'cat', 'pig'], solution: 'dog')
+      expect(puzzle.solution.name).to eq('dog')
+    end
+
+    it 'sets a random solution' do
+      klass = Class.new(Logigram::Base) do
+        constrain 'color', ['red', 'green', 'blue']
+      end
+      # @type [Logigram::Base]
+      puzzle = klass.new(['dog', 'cat', 'pig'])
+      expect(puzzle.pieces).to include(puzzle.solution)
+      expect(['dog', 'cat', 'pig']).to include(puzzle.solution.name)
+    end
+
+    it 'generates premises' do
+      klass = Class.new(Logigram::Base) do
+        constrain 'color', ['red', 'green', 'blue']
+      end
+      # @type [Logigram::Base]
+      puzzle = klass.new(['dog', 'cat', 'pig'])
+      expect(puzzle.premises).not_to be_empty
+    end
+
+    it 'raises an error for insufficient constraint values' do
+      klass = Class.new(Logigram::Base) do
+        constrain 'color', ['red']
+      end
+      expect {
+        klass.new(['dog', 'cat'])
+      }.to raise_error(RuntimeError)
+    end
   end
 end
