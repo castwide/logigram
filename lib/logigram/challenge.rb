146fe64bbf0c0afb9924a6acef88e85900ea027c
Fred Snyder <fsnyder@castwide.com>
2020-08-31 02:19:35 -0400

    More consistent reductions.

--- lib/logigram/challenge.rb
+++ lib/logigram/challenge.rb
@@ -13,37 +13,34 @@ module Logigram
       reductions = []
       previous = nil
       result = []
-      used = []
       @sorted = @puzzle.constraints.values.sort { |a, b| (b.name == @puzzle.solution_term ? 0 : 1) }
       @sorted.each_with_index do |constraint, idx|
-        here = generate_premises(constraint, reductions, previous, used)
-        # reductions = here
-        used.clear
+        here = generate_premises(constraint, reductions, previous)
+        reductions.clear
         result.concat here
         unless @sorted.last == constraint
           here, _ = generate_premise(@puzzle.pieces.last, @sorted[idx + 1], constraint, [@puzzle.pieces.last], here.map { |pr| pr.value })
           result.push here
           reductions.push here
-          used.push here.value
         end
         previous = constraint
       end
-      @clues = result
+      @clues = [result[0]] + result[1..-1].shuffle
     end
 
     # @param constraint [Logigram::Constraint]
     # @param reductions [Array<Logigram::Piece>]
     # @param parent [Logigram::Constraint]
     # @return [Array<Logigram::Premise>]
-    def generate_premises constraint, reductions, parent, used
+    def generate_premises constraint, reductions, parent
       result = []
       passed = false
+      used = []
       @puzzle.pieces[0..-2].each do |piece|
         reductions.push piece if passed
-        next if used.include?(piece.value(constraint.name))
+        # next if used.include?(piece.value(constraint.name))
         premise, last = generate_premise(piece, constraint, parent, reductions, used)
         result.push premise
-        break if premise.identifier && result.length > 1
         used.push last
         passed = true
       end
@@ -51,7 +48,7 @@ module Logigram
     end
 
     def generate_premise piece, constraint, parent, reductions, used
-      value = if reductions.include?(piece) || piece == @puzzle.solution
+      value = if reductions.include?(piece) || (piece == @puzzle.solution && @puzzle.solution_term == constraint.name)
         values = @puzzle.pieces.map { |pc| pc.value(constraint.name) }
         (values - used - [piece.value(constraint.name)]).sample
       else
