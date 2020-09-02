de7923960ae8d107727f8f71e664e18cf0f48f0f
Fred Snyder <fsnyder@castwide.com>
2020-09-02 13:20:32 -0400

    Use require instead of autoload

--- lib/logigram.rb
+++ lib/logigram.rb
@@ -1,9 +1,6 @@
 require "logigram/version"
-
-module Logigram
-  autoload :Piece, 'logigram/piece'
-  autoload :Constraint, 'logigram/constraint'
-  autoload :Premise, 'logigram/premise'
-  autoload :Challenge, 'logigram/challenge'
-  autoload :Base, 'logigram/base'
-end
+require 'logigram/constraint'
+require 'logigram/piece'
+require 'logigram/premise'
+require 'logigram/base'
+require 'logigram/challenge'
