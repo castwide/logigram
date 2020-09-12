a8ec5c35463c4645b28dbc6f28b8f5aecf8fcca4
Fred Snyder <fsnyder@castwide.com>
2020-09-12 10:46:18 -0400

    Minimal challenge algorithm

--- lib/logigram/challenge.rb
+++ lib/logigram/challenge.rb
@@ -9,54 +9,48 @@ module Logigram
     # @param puzzle [Logigram::Base]
     def initialize puzzle
       @puzzle = puzzle
-      reductions = []
-      previous = nil
-      @sorted = @puzzle.constraints.shuffle.sort { |a, b| (b.name == @puzzle.solution_term ? 0 : 1) }
-      constraint_premises = []
-      @sorted.each_with_index do |constraint, idx|
-        here = generate_premises(constraint, reductions, previous)
-        reductions.clear
-        constraint_premises.push here
-        unless @sorted.last == constraint
-          here, _ = generate_premise(@puzzle.pieces.last, @sorted[idx + 1], constraint, [@puzzle.pieces.last], here.map { |pr| pr.value })
-          constraint_premises.last.push here
-          reductions.push here
-        end
-        previous = constraint
-      end
-      flat = constraint_premises.flatten
-      @clues = [flat[0]] + flat[1..-1].shuffle
+      @clues = []
+      @term_values = {}
+      generate_premises
     end
 
     private
 
-    # @param constraint [Logigram::Constraint]
-    # @param reductions [Array<Logigram::Piece>]
-    # @param parent [Logigram::Constraint]
-    # @return [Array<Logigram::Premise>]
-    def generate_premises constraint, reductions, parent
-      result = []
-      passed = false
-      used = []
-      @puzzle.pieces[0..-2].each do |piece|
-        reductions.push piece if passed
-        premise, last = generate_premise(piece, constraint, parent, reductions, used)
-        result.push premise
-        used.push last
-        passed = true
-      end
-      result
+    # @return [Array<Constraint>]
+    def shuffled_constraints
+      @shuffled_constraints ||= (@puzzle.constraints - [@puzzle.constraint(@puzzle.solution_term)]).shuffle + [@puzzle.constraint(@puzzle.solution_term)]
     end
 
-    def generate_premise piece, constraint, parent, reductions, used
-      value = if reductions.include?(piece) || (piece == @puzzle.solution && @puzzle.solution_term == constraint.name)
-        values = @puzzle.pieces.map { |pc| pc.value(constraint.name) }
-        # Sometimes we can wind up here even though the piece's current value is the only available option
-        (values - used - [piece.value(constraint.name)]).sample || piece.value(constraint.name)
-      else
-        piece.value(constraint.name)
+    def remove_value term, value
+      @term_values[term] ||= @puzzle.term_values(term)
+      @term_values[term].delete value
+    end
+
+    def sample_value term, except = nil
+      @term_values[term] ||= @puzzle.term_values(term)
+      (@term_values[term] - [except]).sample
+    end
+
+    def generate_premises
+      last_constraint = nil
+      shuffled_constraints[0..-2].each do |constraint|
+        @puzzle.pieces.shuffle[0..-2].each_with_index do |piece, index|
+          @clues.push generate_premise(piece, constraint, last_constraint, index < @puzzle.pieces.length - 2)
+        end
+        last_constraint = constraint
       end
-      [Logigram::Premise.new(piece, constraint, value, parent), value]
+      (@puzzle.pieces - [@puzzle.solution]).shuffle.each_with_index do |piece, index|
+        @clues.push generate_premise(piece, shuffled_constraints.last, last_constraint, index < @puzzle.pieces.length - 2)
+      end
+      @clues = [@clues[0]] + @clues[1..-1].shuffle
+    end
+
+    # @param piece [Logigram::Piece]
+    # @param constraint [Logigram::Constraint]
+    def generate_premise piece, constraint, identifier, affirm
+      value = affirm ? piece.value(constraint.name) : sample_value(constraint.name)
+      remove_value constraint.name, value
+      Logigram::Premise.new(piece, constraint, value, identifier)
     end
   end
 end
