c146cd449acc5a1d27fc41eb037757769e45f71a
Fred Snyder <fsnyder@castwide.com>
2021-11-07 11:50:18 -0500

    Reserve selection

--- lib/logigram/base.rb
+++ lib/logigram/base.rb
@@ -202,12 +202,12 @@ module Logigram
           if selected
             repo[c.name] = [selected_values[c.name]]
           elsif c.name == fixed_term
-            repo[c.name] = limit_available_values(c, selected_values[fixed_term])
+            repo[c.name] = limit_available_values(c, selected_values[fixed_term], selected)
           else
-            repo[c.name] = limit_available_values(c, nil)
+            repo[c.name] = limit_available_values(c, nil, false)
           end
         else
-          repo[c.name] = limit_available_values(c, nil)
+          repo[c.name] = limit_available_values(c, nil, selected)
         end
       end
       repo
@@ -216,9 +216,9 @@ module Logigram
     # @param constraint [Constraint]
     # @param exception [String]
     # @return [Array<String>]
-    def limit_available_values constraint, exception
-      available = constraint.values - [exception]
-      filtered = available - pieces.map { |p| p.value(constraint.name) }
+    def limit_available_values constraint, exception, selected
+      available = (selected ? constraint.reserves : constraint.values) - [exception]
+      filtered = available - [@solution&.value(constraint.name)] - pieces.map { |p| p.value(constraint.name) }
       filtered.empty? ? available : filtered
     end
 
