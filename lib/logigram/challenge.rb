3748d0fa2979367c73d9a8e08f89dd8bb8fc42c9
Fred Snyder <fsnyder@castwide.com>
2025-05-13 22:39:11 -0400

    Refactor premise generation

--- lib/logigram/challenge.rb
+++ lib/logigram/challenge.rb
@@ -19,56 +19,63 @@ module Logigram
 
     # @return [Array<Premise>]
     def premises
-      @premises ||= generate_premises
+      @premises ||= (generate_unique_premises + generate_ambiguous_premises).shuffle
     end
     alias clues premises
 
     private
 
-    def generate_premises
-      result = []
+    def generate_unique_premises
       last_constraint = nil
-      # The first premise will be affirmative and specific, but since it
-      # shouldn't be derived from a determinant, it can be the solution
-      pieces = puzzle.pieces.shuffle
-      (unique_constraints + unique_determinants).each do |constraint|
-        result.concat generate_unique_premises(constraint, pieces, last_constraint)
-        shuffle_pieces!(pieces)
-        last_constraint = constraint
+      (unique_constraints + unique_determinants).flat_map do |constraint|
+        next_constraint_premises(constraint, last_constraint).tap do
+          reshuffle_pieces
+          last_constraint = constraint
+        end
       end
-      (ambiguous_constraints + ambiguous_determinants).each do |constraint|
-        result.concat generate_unique_premises(constraint, pieces, random_unique_constraint)
-        shuffle_pieces!(pieces)
-      end
-      result
     end
 
-    # @return [void]
-    def shuffle_pieces!(pieces)
-      last = pieces.pop
-      pieces.shuffle!
-      pieces.unshift last
-      pieces.delete puzzle.solution
-      pieces.push puzzle.solution
+    def generate_ambiguous_premises
+      (ambiguous_constraints + ambiguous_determinants).flat_map do |constraint|
+        next_constraint_premises(constraint, pick_random_unique_constraint).tap { reshuffle_pieces }
+      end
+    end
+
+    # @return [Array<Piece>]
+    def shuffled_pieces
+      @shuffled_pieces ||= puzzle.pieces.shuffle
+    end
+
+    def reshuffle_pieces
+      # Constraint premise generation doesn't make a premise for the last
+      # piece in the list. Move it to the front before the next iteration to
+      # ensure that each piece (with the possible exception of the solution)
+      # gets at least one premise
+      last = shuffled_pieces.pop
+      shuffled_pieces.shuffle!
+      shuffled_pieces.unshift last
+      # Avoid making more than one premise for the solution
+      shuffled_pieces.delete puzzle.solution
+      shuffled_pieces.push puzzle.solution
     end
 
     # @param constraint [Constraint]
     # @param pieces [Array<Piece>]
     # @param last_constraint [Constraint, nil]
-    def generate_unique_premises(constraint, pieces, last_constraint)
-      pieces[0..-2].map.with_index do |piece, idx|
-        if idx.zero? || difficulty == :easy || (difficulty == :medium && idx.even?)
-          Premise.new(piece, constraint, piece.value(constraint.name), last_constraint)
-        else
-          Premise.new(piece, constraint, pieces[idx + 1].value(constraint.name), last_constraint)
-        end
+    def next_constraint_premises(constraint, last_constraint)
+      shuffled_pieces[0..-2].map.with_index do |piece, idx|
+        premise_value = should_be_easy?(idx) ? piece.value(constraint.name) : shuffled_pieces[-idx].value(constraint.name)
+        Premise.new(piece, constraint, premise_value, last_constraint)
       end
     end
 
+    def should_be_easy?(idx)
+      idx.zero? || difficulty == :easy || (difficulty == :medium && idx.even?)
+    end
+
     def unique_determinants
-      @unique_determinants ||= puzzle.determinants.select do |det|
-        sol_val = puzzle.solution.value(det.name)
-        puzzle.pieces.select { |piece| piece.value(det.name) == sol_val }.one?
+      @unique_determinants ||= puzzle.determinants.select do |con|
+        puzzle.pieces.map { |piece| piece.value(con.name) }.uniq.length == puzzle.pieces.length
       end
     end
 
@@ -86,12 +93,17 @@ module Logigram
       @ambiguous_constraints ||= puzzle.constraints - puzzle.determinants - unique_constraints
     end
 
-    def random_unique_constraint
-      @random_unique_constraints ||= (unique_constraints + unique_determinants).shuffle
-      if @random_unique_constraints.empty?
-        @random_unique_constraints.replace((unique_constraints + unique_determinants).shuffle)
-      end
-      @random_unique_constraints.pop
+    def pick_random_unique_constraint
+      random_unique_constraints.replace(shuffle_unique_restraints) if random_unique_constraints.empty?
+      random_unique_constraints.pop
+    end
+
+    def random_unique_constraints
+      @random_unique_constraints ||= shuffle_unique_restraints
+    end
+
+    def shuffle_unique_restraints
+      (unique_constraints + unique_determinants).shuffle
     end
   end
 end
