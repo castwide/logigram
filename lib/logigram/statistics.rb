4877b07819b4a341d567765a1d831188a55bd026
Fred Snyder <fsnyder@castwide.com>
2025-05-14 05:12:37 -0400

    Datasets -> Statistics

--- lib/logigram/statistics.rb
+++ lib/logigram/statistics.rb
@@ -1,42 +1,22 @@
 # frozen_string_literal: true
 
 module Logigram
-  # A data summary about the pieces and premises of a puzzle.
+  # Methods for collecting puzzle data.
   #
-  # @param puzzle [Logigram::Base]
-  # @param subject [String]
-  # @param plural [String]
-  class Statistics
-    def initialize(puzzle, subject: 'thing', plural: "#{subject}s")
-      @puzzle = puzzle
-      @subject = subject
-      @plural = plural
-    end
+  module Statistics
+    module_function
 
-    # @return [Hash]
-    def raw_data
-      @raw_data ||= Datasets.constraint_tables(@puzzle)
-    end
-
-    # @return [Array<String>]
-    def statements
-      @statements ||= generate_statements
-    end
-
-    private
-
-    def noun(qty)
-      qty == 1 ? @subject : @plural
-    end
-
-    def generate_statements
-      lines = []
-      raw_data.each_pair do |con, values|
-        values.each_pair do |val, qty|
-          lines.push "#{qty} #{noun(qty)} #{con.predicate(val, qty)}"
+    # @param puzzle [Puzzle]
+    # @return [Hash{Constraint => Hash{String => Integer}}]
+    def constraint_tables(puzzle)
+      puzzle.constraints.map do |con|
+        values = {}
+        puzzle.pieces.each do |pc|
+          values[pc.value(con.name)] ||= 0
+          values[pc.value(con.name)] += 1
         end
-      end
-      lines
+        [con, values]
+      end.to_h
     end
   end
 end
